553d8443267ccaeb2ec14dc0f2afd26514633dd7
Jon Dotsoy <hi@jon.soy>
2016-03-28 18:30:36 -0300

    Script allow create a bin `cli.js`

--- script/generate_bin_cli.js
+++ script/generate_bin_cli.js
@@ -4,28 +4,58 @@ var path = require("path");
 var async = require("async");
 
 var file = path.normalize(__dirname + "/../bin/cli.js");
+var dirbin = path.normalize(__dirname + "/../bin");
+var fp = null;
+var existsDirectoryBin = false; 
 
-fs.open(file, 'w+', 766, function(e, id) {
-	async
-	.series([
-		function(next) {
-			fs
-				.write(id, [
-						"#!/usr/bin/env node",
-						"",
-						"require(\"../lib/cli\").cli(process.argv.slice(2));",
-						"",
-					].join("\n"),
-					null,
-					'utf8',
-					function(err, written, buffer) {
-						next(err, written);
-					});
-		},
-	], function() {
-		fs.close(id, function() {
+async
+.series([
+	// Check directory and create
+	function (next) {
+		fs.stat(dirbin, function (err, statDir) {
+			if (err) {
+				fs.mkdir(dirbin, function (err) {
+					next(err);
+				});
+			} else {
+				next();
+			}
+		});
+	},
+	// Open file
+	function(next) {
+		fs.open(file, 'w', 766, function(err, id) {
+			fp = id;
+			next(err, id);
+		});
+	},
+
+	// Write file
+	function(next) {
+		fs
+			.write(fp, [
+					"#!/usr/bin/env node",
+					"",
+					"require(\"../lib/cli\").cli(process.argv.slice(2));",
+					"",
+				].join("\n"),
+				null,
+				'utf8',
+				function(err, written, buffer) {
+					next(err, written);
+				});
+	},
+
+	// Close file
+	function(next) {
+		fs.close(fp, function(err) {
+			next(err);
 			console.log(`File Create ${file}.`);
 		});
-	});
-
+	}
+], function (err) {
+	if (err) {
+		console.log(err.stack);
+		process.exit(1);
+	}
 });
